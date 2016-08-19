e18a83a02583f1066d21c8044f9e0cc83caf311a
Arnaud Auroux <arnaud@auroux.me>
2016-08-19 10:24:24 +0200

    Add ClaimsIdentity helper

---
+++ src/GeekLearning.Test.Integration/Helpers/ClaimsIdentityHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace System.Security.Claims
+{
+    using System.Linq;
+
+    public static class ClaimsIdentityHelper
+    {
+        public static void ReplaceClaim(this ClaimsIdentity identity, string type, string value)
+        {
+            identity.TryRemoveClaim(identity.Claims.FirstOrDefault(c => c.Type == type));
+            identity.AddClaim(new Claim(type, value));
+        }
+
+        public static void ReplaceNameIdentifierClaim(this ClaimsIdentity identity, string value)
+        {
+            identity.ReplaceClaim("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", value);
+        }
+    }
+}
