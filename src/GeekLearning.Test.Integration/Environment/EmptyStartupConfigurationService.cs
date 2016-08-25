46d6cea686c615528ec7861d6b9a3587c6a2080f
Arnaud Auroux <arnaud@auroux.me>
2016-08-25 18:40:41 +0200

    Add empty Startup configuration service implementation

---
+++ src/GeekLearning.Test.Integration/Environment/EmptyStartupConfigurationService.cs
@@ -0,0 +1,31 @@
+﻿namespace GeekLearning.Test.Integration.Environment
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using System;
+
+    public class StartupConfigurationService : IStartupConfigurationService
+    {
+        public IServiceProvider ServiceProvider
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) { }
+
+        public void ConfigureEnvironment(IHostingEnvironment env) { }
+
+        public void ConfigureService(IServiceCollection services, IConfigurationRoot configuration) { }
+
+        public void RegisterExternalStartupConfigured(Action callback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
