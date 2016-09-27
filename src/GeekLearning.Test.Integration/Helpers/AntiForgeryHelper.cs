2342126fa603d69a48dffa5572fafd23fa452c0a
arnau <arnau@DESKTOP-FU5JJ2G>
2016-09-27 12:14:56 +0200

    Add anti forgery helpers

---
+++ src/GeekLearning.Test.Integration/Helpers/AntiForgeryHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace GeekLearning.Test.Integration.Helpers
+{
+    using System;
+    using System.Net.Http;
+    using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
+
+    // http://www.stefanhendriks.com/2016/05/11/integration-testing-your-asp-net-core-app-dealing-with-anti-request-forgery-csrf-formdata-and-cookies/
+    public class AntiForgeryHelper
+    {
+        public static string ExtractAntiForgeryToken(string htmlResponseText)
+        {
+            if (htmlResponseText == null) throw new ArgumentNullException("htmlResponseText");
+
+            System.Text.RegularExpressions.Match match = Regex.Match(htmlResponseText, @"\<input name=""__RequestVerificationToken"" type=""hidden"" value=""([^""]+)"" \/\>");
+            return match.Success ? match.Groups[1].Captures[0].Value : null;
+        }
+
+        public static async Task<string> ExtractAntiForgeryToken(HttpResponseMessage response)
+        {
+            string responseAsString = await response.Content.ReadAsStringAsync();
+            return await Task.FromResult(ExtractAntiForgeryToken(responseAsString));
+        }
+    }
+}
---
+++ src/GeekLearning.Test.Integration/Helpers/PostRequestHelper.cs
@@ -0,0 +1,30 @@
+﻿namespace GeekLearning.Test.Integration.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    // http://www.stefanhendriks.com/2016/05/11/integration-testing-your-asp-net-core-app-dealing-with-anti-request-forgery-csrf-formdata-and-cookies/
+    public class PostRequestHelper
+    {
+        public static HttpRequestMessage Create(String path, Dictionary<string, string> formPostBodyData)
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, path)
+            {
+                Content = new FormUrlEncodedContent(ToFormPostData(formPostBodyData))
+            };
+            return httpRequestMessage;
+        }
+
+        public static List<KeyValuePair<string, string>> ToFormPostData(Dictionary<string, string> formPostBodyData)
+        {
+            List<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
+            formPostBodyData.Keys.ToList().ForEach(key =>
+            {
+                result.Add(new KeyValuePair<string, string>(key, formPostBodyData[key]));
+            });
+            return result;
+        }
+    }
+}
