f1a6c9907a62947dac60d6a78c8e2036646df487
Cyprien Autexier <2660262+sandorfr@users.noreply.github.com>
2018-08-01 16:39:25 +1000

    Support for netcoreapp21

--- sample/Sample.Test.Splecflow/Mvc/GetBlogs/GetBlogs.feature.cs
+++ sample/Sample.Test.Splecflow/Mvc/GetBlogs/GetBlogs.feature.cs
@@ -23,8 +23,22 @@ namespace SpecFlow.GeneratedTests.Mvc.GetBlogs
         
         private static TechTalk.SpecFlow.ITestRunner testRunner;
         
+        private Microsoft.VisualStudio.TestTools.UnitTesting.TestContext _testContext;
+        
 #line 1 "GetBlogs.feature"
 #line hidden
+        
+        public virtual Microsoft.VisualStudio.TestTools.UnitTesting.TestContext TestContext
+        {
+            get
+            {
+                return this._testContext;
+            }
+            set
+            {
+                this._testContext = value;
+            }
+        }
         
         [Microsoft.VisualStudio.TestTools.UnitTesting.ClassInitializeAttribute()]
         public static void FeatureSetup(Microsoft.VisualStudio.TestTools.UnitTesting.TestContext testContext)
@@ -60,6 +74,7 @@ namespace SpecFlow.GeneratedTests.Mvc.GetBlogs
         public virtual void ScenarioSetup(TechTalk.SpecFlow.ScenarioInfo scenarioInfo)
         {
             testRunner.OnScenarioStart(scenarioInfo);
+            testRunner.ScenarioContext.ScenarioContainer.RegisterInstanceAs<Microsoft.VisualStudio.TestTools.UnitTesting.TestContext>(TestContext);
         }
         
         public virtual void ScenarioCleanup()
@@ -76,7 +91,7 @@ namespace SpecFlow.GeneratedTests.Mvc.GetBlogs
         }
         
         [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethodAttribute()]
-
+        
         [Microsoft.VisualStudio.TestTools.UnitTesting.TestPropertyAttribute("FeatureTitle", "Get blogs")]
         public virtual void Specflow_Mvc_GetBlogsShouldBeOk()
         {
